e80322759ab76d24d0e7304917b6956be79a455b
Mikhail <87110754+mixx3@users.noreply.github.com>
2022-03-06 12:18:31 +0300

    Create random_number_game.c

---
+++ random_number_game.c
@@ -0,0 +1,22 @@
+//task 4 #8 P_Mikhail
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+int main(){
+    srand(time(NULL));
+    int num = rand() % 306;
+    num -= num % 6;
+    
+    int counter = 1, userNum = 301, guess = 0;
+    do {
+        scanf("%d", &userNum);
+        printf(userNum < num ? "Bigger\n" : userNum == num? "You win!\n%d attempts" : "Smaller\n",counter);
+        if (userNum == num) {
+            guess ++;
+        }
+        ++counter;
+    }
+    while (guess == 0);
+    return 0;
+}
